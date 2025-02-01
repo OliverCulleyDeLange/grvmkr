b8345dc7db6d93ecb69a1c71a688f70b66dfa84c
Oliver Culley de Lange <olly@oliverdelange.co.uk>
2025-02-01 13:25:07 -0800

    Selectable fixed 8x8 grid

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,11 @@
 <h1>GrvMkr</h1>
 
 <script lang="ts">
+	import GridCell from "./GridCell.svelte";
+
     let audioContext: AudioContext | null = null;
     let audioBuffer: AudioBuffer | null = null;
+    let grid: Array<Array<boolean>> = Array.from({ length: 8 }, () => Array(8).fill(0));
   
     async function loadSound(): Promise<void> {
       if (!audioContext) {
@@ -23,7 +26,26 @@
       source.connect(audioContext.destination);
       source.start(0);
     }
+
+    async function play() : Promise<void> {
+
+    }
+
+    async function onTapGridCell(row: number, col: number): Promise<void> {
+        // console.log(`Tapped row ${row} col ${col}`)
+        let currentValue = grid[row][col] 
+        grid[row][col] = !currentValue
+    }
+
   </script>
   
-  <button on:click={playSound}>Play Sound</button>
+  <button on:click={play}>Play</button>
+
+  <div class="grid grid-cols-8 gap-1 w-64">
+    {#each Array(8) as _, row}
+        {#each Array(8) as _, col}
+            <GridCell text={col + 1} selected={grid[row][col]} onTap={() => onTapGridCell(row, col)}/>
+        {/each}
+    {/each}
+  </div>
   
---
+++ src/routes/GridCell.svelte
@@ -0,0 +1,7 @@
+<script lang='ts'>
+    let { text, selected, onTap} = $props();
+</script>
+
+<button onclick={onTap} class="w-8 h-8 {selected ? "bg-green-300" : "bg-gray-300"} flex items-center justify-center border border-gray-400">
+   {text}
+</button>
