c3daf9db80c14c4b4f54b0da871a9ab2c48146af
Oliver Culley de Lange <olly@oliverdelange.co.uk>
2025-02-01 13:53:56 -0800

    Print beat and bar in time

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,8 @@
 	let audioBuffer: AudioBuffer | null = null;
 	let grid: Array<Array<boolean>> = Array.from({ length: 8 }, () => Array(8).fill(0));
 	let playing = false;
-	let bpm: number = 60;
+	let bpm: number = 120;
+	let beat: number = 0;
 	let playingIntervalId: number | undefined = undefined;
 
 	async function loadSound(): Promise<void> {
@@ -32,16 +33,22 @@
 		if (playing) {
 			clearInterval(playingIntervalId); // Stop after printing the whole string
 			playingIntervalId = undefined;
-            playing = false;
+			playing = false;
 		} else {
 			// Every beat takes this number of ms
 			const bpmIntervalMs = 60000 / bpm;
 
+			onBeat(0, beat++);
 			playingIntervalId = setInterval(() => {
-				playSound();
+				let newBeat = beat++;
+				onBeat(Math.floor(newBeat / 8), newBeat % 8);
 			}, bpmIntervalMs);
-            playing = true;
+			playing = true;
 		}
+	}
+
+	function onBeat(bar: number, beat: number) {
+		console.log(`Bar ${bar}, Beat ${beat}`);
 	}
 
 	async function onTapGridCell(row: number, col: number): Promise<void> {
@@ -53,11 +60,11 @@
 
 <h1>GrvMkr</h1>
 
-<button on:click={togglePlaying}>{playing ? "Stop" : "Play"}</button>
+<button on:click={togglePlaying}>{playing ? 'Stop' : 'Play'}</button>
 <input type="number" bind:value={bpm} min="20" max="200" />
 
 <div class="grid w-64 grid-cols-8 gap-1">
-	{#each Array(8) as _, row}
+	{#each Array(1) as _, row}
 		{#each Array(8) as _, col}
 			<GridCell text={col + 1} selected={grid[row][col]} onTap={() => onTapGridCell(row, col)} />
 		{/each}
