dd97ad6e4d499fb40f514ac7f46903e488c632b3
Oliver Culley de Lange <olly@oliverdelange.co.uk>
2025-05-20 00:32:49 +0100

    PWA fix please work plzzzz

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
+	import { base } from '$app/paths';
 	import '../app.css';
 	let { children } = $props();
 </script>
+
+<svelte:head>
+	<link rel="manifest" href={`${base}/manifest.json`} />
+</svelte:head>
 
 {@render children()}
