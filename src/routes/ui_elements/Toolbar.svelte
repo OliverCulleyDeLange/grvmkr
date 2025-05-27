8f162aa83d8e0a94fbbcb5b5a2d41adacdc5d73c
Oliver Culley de Lange <olly@oliverdelange.co.uk>
2025-05-27 21:13:59 +0100

    Position error close button properly

--- src/routes/ui_elements/Toolbar.svelte
+++ src/routes/ui_elements/Toolbar.svelte
@@ -92,8 +92,8 @@
 {#if toolbarUi.errors.length > 0}
 	<div class="my-4 flex flex-col gap-2">
 		{#each toolbarUi.errors as error}
-			<div class="alert alert-error">
-				{error.message}
+			<div class="alert alert-error flex items-start justify-between gap-4">
+				<span>{error.message}</span>
 				<button
 					class="btn btn-circle btn-ghost btn-xs"
 					onclick={() => onEvent({ event: ToolbarEvent.DismissError, id: error.id })}
