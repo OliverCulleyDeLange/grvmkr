38f9e2b2de5c516b2ce599e4c905e5c977d2ddbd
Oliver Culley de Lange <olly@oliverdelange.co.uk>
2025-05-29 13:53:22 +0100

    Add default instruments

--- src/lib/domain/model/default_instruments.ts
+++ src/lib/domain/model/default_instruments.ts
@@ -16,56 +16,57 @@ export const defaultInstrumentConfig: InstrumentConfig = {
 
 export const defaultInstruments: Array<InstrumentConfig> = [
 	{
-		name: 'Hi Hat',
+		name: 'Surdo L',
 		hitTypes: [
-			{ ...defaultHitType, key: 'C', description: 'Closed', audioFileName: 'hat_closed.wav' },
-			{
-				...defaultHitType,
-				key: 'O',
-				description: 'Open',
-				audioFileName: 'hat_open.wav',
-				volume: defaultVolume
-			}
+			{ ...defaultHitType, key: 'X', description: '', audioFileName: 'surdo_l.wav' },
+			{ ...defaultHitType, key: 'm', description: 'Muted', audioFileName: 'surdo_l_muted.wav' },
 		]
 	},
 	{
-		name: 'Snare',
-		hitTypes: [{ ...defaultHitType, key: 'X', description: '', audioFileName: 'snare.wav' }]
-	},
-	{
-		name: 'Kick',
-		hitTypes: [{ ...defaultHitType, key: 'X', description: '', audioFileName: 'kick.wav' }]
-	},
-	{
-		name: 'Surdo L',
-		hitTypes: [{ ...defaultHitType, key: 'X', description: '', audioFileName: 'surdo_l.wav' }]
-	},
-	{
 		name: 'Surdo M',
-		hitTypes: [{ ...defaultHitType, key: 'X', description: '', audioFileName: 'surdo_m.wav' }]
+		hitTypes: [
+			{ ...defaultHitType, key: 'X', description: '', audioFileName: 'surdo_m.wav' },
+			{ ...defaultHitType, key: 'm', description: 'Muted', audioFileName: 'surdo_m_muted.wav' },
+		]
 	},
 	{
 		name: 'Surdo H',
-		hitTypes: [{ ...defaultHitType, key: 'X', description: '', audioFileName: 'surdo_h.wav' }]
+		hitTypes: [
+			{ ...defaultHitType, key: 'X', description: '', audioFileName: 'surdo_h.wav' },
+			{ ...defaultHitType, key: 'm', description: 'Muted', audioFileName: 'surdo_h_muted.wav' },
+		]
 	},
 	{
 		name: 'Repinique',
 		hitTypes: [
 			{ ...defaultHitType, key: 'X', description: 'Hit', audioFileName: 'repinique_hit.wav' },
 			{ ...defaultHitType, key: 'r', description: 'Rim', audioFileName: 'repinique_rim.wav' },
-			{ ...defaultHitType, key: 'H', description: 'Hand', audioFileName: 'repinique_hand.wav' }
+			{ ...defaultHitType, key: 'H', description: 'Hand', audioFileName: 'repinique_hand.wav' },
+			{ ...defaultHitType, key: 'D', description: 'Double', audioFileName: 'repinique_double.wav' },
+			{ ...defaultHitType, key: 'R', description: 'Roll', audioFileName: 'repinique_roll.wav' }
 		]
 	},
 	{
 		name: 'Caixa',
 		hitTypes: [
 			{ ...defaultHitType, key: 'X', description: 'Accent', audioFileName: 'caixa.wav' },
-			{ ...defaultHitType, key: 'x', description: 'Ghost', audioFileName: 'caixa_ghost.wav' }
+			{ ...defaultHitType, key: 'x', description: 'Ghost', audioFileName: 'caixa_ghost.wav' },
+			{ ...defaultHitType, key: 'r', description: 'Roll', audioFileName: 'caixa_roll.wav' },
 		]
 	},
 	{
 		name: 'Tamborim',
-		hitTypes: [{ ...defaultHitType, key: 'X', description: '', audioFileName: 'tamborim.wav' }]
+		hitTypes: [
+			{ ...defaultHitType, key: 'X', description: '', audioFileName: 'tamborim.wav' },
+			{ ...defaultHitType, key: 'x', description: '', audioFileName: 'tamborim_ghost.wav' },
+		]
+	},
+	{
+		name: 'Agogo Bell',
+		hitTypes: [
+			{ ...defaultHitType, key: 'H', description: 'High', audioFileName: 'agogo_bell_high.wav' },
+			{ ...defaultHitType, key: 'L', description: 'Low', audioFileName: 'agogo_bell_low.wav' },
+		]
 	},
 	{
 		name: 'Timba',
@@ -77,8 +78,20 @@ export const defaultInstruments: Array<InstrumentConfig> = [
 	{
 		name: 'Shaker',
 		hitTypes: [
-			{ ...defaultHitType, key: 'X', description: '', audioFileName: 'shaker_a.wav' },
-			{ ...defaultHitType, key: 'x', description: '', audioFileName: 'shaker_g.wav' }
+			{ ...defaultHitType, key: 'X', description: 'Accent', audioFileName: 'shaker_accent.wav' },
+			{ ...defaultHitType, key: 'x', description: 'Ghost', audioFileName: 'shaker_ghost.wav' }
+		]
+	},
+	{
+		name: 'Oi',
+		hitTypes: [
+			{ ...defaultHitType, key: 'Oi', description: '', audioFileName: 'oi.wav' },
+		]
+	},
+	{
+		name: 'Whistle',
+		hitTypes: [
+			{ ...defaultHitType, key: 'W', description: '', audioFileName: 'whistle.wav' },
 		]
 	}
 ];
