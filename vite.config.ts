86bbdfb6f0469904012e009c612c5d2f2642e270
Oliver Culley de Lange <olly@oliverdelange.co.uk>
2025-06-01 19:46:37 +0100

    Remove vite pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-	// resolve: {
-	// 	extensions: ['.ts', '.js', '.svelte'],
-	// },
 	plugins: [
 		sveltekit(),
-		VitePWA({
-			registerType: 'autoUpdate',
-			manifest: {
-				name: 'Your App',
-				short_name: 'App',
-				start_url: '/',
-				display: 'standalone',
-				background_color: '#ffffff',
-				theme_color: '#121212',
-				icons: [
-					{
-						src: '/icons/icon-192.png',
-						sizes: '192x192',
-						type: 'image/png'
-					},
-					{
-						src: '/icons/icon-512.png',
-						sizes: '512x512',
-						type: 'image/png'
-					}
-				]
-			}
-		})
 	]
 });
