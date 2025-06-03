2790c5e200ddeb64bd3b76b12a98dd1d598e1853
Oliver Culley de Lange <olly@oliverdelange.co.uk>
2025-06-03 11:30:06 +0100

    Fix base url in prod?

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
@@ -16,7 +18,7 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: dev ? '' : "/grvmkr"
 		}
 	}
 };
